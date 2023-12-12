7c42ab06c746a635b48d2174d36aa608872221ed
Eduardo Gil <eggom84@gmail.com>
2023-12-12 13:18:52 +0000

    v0.0.5 -- os_listdir_practice file

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@
 # A package to practice merging and branching
 ###
 
-__version__ = "T.B.C"
+__version__ = "0.0.5"
 
 
 #HISTORY
 
-#vT.B.C -- os_listdir_practice file
+#v0.0.5 -- os_listdir_practice file
 #v0.0.3 -- os_getcwd_practice file
 #v0.0.2 -- os_practice folder & os_getenv_practice file
 #v0.0.1 -- Creation of __init__.py
