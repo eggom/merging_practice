d1c00ef1f56ad881f7dec96962dd9893d8500798
Eduardo Gil <eggom84@gmail.com>
2023-12-12 10:44:21 +0000

    v0.0.3 -- os_getcwd_practice file

--- __init__.py
+++ __init__.py
@@ -2,10 +2,11 @@
 # A package to practice merging and branching
 ###
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 #HISTORY
 
+#v0.0.3 -- os_getcwd_practice file
 #v0.0.2 -- os_practice folder & os_getenv_practice file
 #v0.0.1 -- Creation of __init__.py
