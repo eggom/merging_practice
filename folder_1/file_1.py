f34c769f445c475306563ec910cdcdf81fe6cd15
Eduardo Gil <eggom84@gmail.com>
2023-12-12 10:24:16 +0000

    folder_1 - file_1 created

---
+++ folder_1/file_1.py
@@ -0,0 +1,12 @@
+# This is file 1
+
+import os
+
+# Get the value of 'HOME' 
+# environment variable 
+key = 'HOME'
+value = os.getenv(key) 
+  
+# Print the value of 'HOME' 
+# environment variable 
+print("Value of 'HOME' environment variable :", value)
